f93e2072b0b3546583e0d89268102dee87ad0d5d
Anderson Felipe <78077613+Ahegys@users.noreply.github.com>
2022-05-03 09:35:00 -0300

    Update main.js

--- main.js
+++ main.js
@@ -18,3 +18,15 @@ const carrossel = () => {
 	img.style.transition = `1s`;
 }
 //setInterval(carrossel, 1000);
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () =>{
+    if(!menuOpen){
+        menuBtn.classList.add('open')
+        menuOpen = true;
+    }
+    else{
+        menuBtn.classList.remove('open')
+        menuOpen = false;
+    }
+})
